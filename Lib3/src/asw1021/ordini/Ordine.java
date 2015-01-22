66c3b9abcbdb839f4b794b6ca6f8e1f36eb0769d
Beatrice Mezzapesa <beatrice.mezzapesa@studio.unibo.it>
2015-01-22 17:40:41 +0100

    Documentazione Lib3

--- Lib3/src/asw1021/ordini/Ordine.java
+++ Lib3/src/asw1021/ordini/Ordine.java
@@ -10,21 +10,51 @@ import java.util.ArrayList;
 
 /**
  *
- * @author Alessia
+ * @author Mezzapesa Beatrice, Papini Alessia, Pontellini Lorenzo
+ */
+/** Quest'interfaccia viene usata come modello base per la gestione degli ordini
  */
 public interface Ordine {
     
+    /** Ottengo l'identificativo dell'ordine
+     * @return String che appunto mi individua l'ordine relativo
+     */
+    
     public String getId();
     
+    /** Ottengo il nome del cliente che ha richiesto l'ordine
+     * @return String con il nome dello user
+     */
     public String getUser();
+    
+    /** Una volta confermato l'ordine dal cliente, aggiungo le pizze da lui scelte
+     * @param p è una pizza o un elenco di pizze
+     */
     
     public void addPizza(pizza p);
     
+    /** Ottengo le pizze richieste nell'ordine da parte del cliente
+     * @return Arraylist<pizza> che contiene o una pizza o l'elenco di pizze
+     * presenti nell'ordine
+     */
+    
     public ArrayList<pizza> getPizze();
+    
+    /** Ottengo che tipo di richiesta ha fatto il cliente
+     * @return String che può essere asporto, ritiro o prenotazione
+     */
     
     public String getType();
     
-    public String getState();
+    /** Ottengo lo stato dell'ordine, cioè se è già stato processato o meno
+     * @return Boolean che mi dice true o false
+     */
     
-    public void setState(String state);
+    public Boolean getState();
+    
+    /** Il cuoco setta lo stato dell'ordine, cioè se le pizze sono pronte o meno
+     * @param state contiene al suo interno il valore true o false
+     */
+    
+    public void setState(Boolean state);
 }
--- Lib3/src/asw1021/ordini/OrdineCliente.java
+++ Lib3/src/asw1021/ordini/OrdineCliente.java
@@ -10,51 +10,92 @@ import java.util.ArrayList;
 
 /**
  *
- * @author Alessia
+ * @author Mezzapesa Beatrice, Papini Alessia, Pontellini Lorenzo
+ */
+/** Classe usata per la gestione degli ordini fatti dai clienti 
  */
 public class OrdineCliente implements Ordine{
     
-    String id, user, type, state;
+    String id, user, type;
+    Boolean state;
     ArrayList<pizza> elencoPizze;
     
-    public OrdineCliente(String user, String id, String type, String state){
+    /** Setto il nome utente, l'identificativo dell'ordine, il tipo di richiesta
+     * e lo stato dell'ordine
+     * @param user
+     * @param id
+     * @param type
+     * @param state 
+     */
+    
+    public OrdineCliente(String user, String id, String type, Boolean state){
         this.user = user;
         this.id = id;
         this.type = type;
         this.state = state;
     }
+    
+    /** Ottengo l'identificativo dell'ordine
+     * @return String che appunto mi individua l'ordine relativo
+     */
+    
     @Override
     public String getId() {
         return id;        
     }
+    
+    /** Ottengo il nome del cliente che ha richiesto l'ordine
+     * @return String con il nome dello user
+    */
 
     @Override
     public String getUser() {
         return user;
     }
     
+    /** Una volta confermato l'ordine dal cliente, aggiungo le pizze da lui scelte
+     * @param p è una pizza o un elenco di pizze
+     */
+    
     @Override
     public void addPizza(pizza p) {
         elencoPizze.add(p);
     }
     
+    /** Ottengo le pizze richieste nell'ordine da parte del cliente
+     * @return Arraylist<pizza> che contiene o una pizza o l'elenco di pizze
+     * presenti nell'ordine
+     */
+    
     @Override
     public ArrayList<pizza> getPizze() {
         return elencoPizze;
     }
+    
+    /** Ottengo che tipo di richiesta ha fatto il cliente
+     * @return String che può essere asporto, ritiro o prenotazione
+     */
 
     @Override
     public String getType() {
         return type;
     }
 
+    /** Ottengo lo stato dell'ordine, cioè se è già stato processato o meno
+     * @return Boolean che mi dice true o false
+     */
+    
     @Override
-    public String getState() {
+    public Boolean getState() {
         return state;
     }
+    
+    /** Il cuoco setta lo stato dell'ordine, cioè se le pizze sono pronte o meno
+     * @param state contiene al suo interno il valore true o false
+     */
 
     @Override
-    public void setState(String state) {
+    public void setState(Boolean state) {
         this.state = state;
     }
     

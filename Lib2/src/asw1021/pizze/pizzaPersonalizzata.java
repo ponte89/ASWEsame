f7eeb0ca10d88464e1758066b900bb8419f224e9
Beatrice Mezzapesa <beatrice.mezzapesa@studio.unibo.it>
2015-01-22 15:54:58 +0100

    Documentazione Lib2

--- Lib2/src/asw1021/pizze/pizzaPersonalizzata.java
+++ Lib2/src/asw1021/pizze/pizzaPersonalizzata.java
@@ -8,7 +8,10 @@ import java.util.ArrayList;
  */
 /**
  *
- * @author Lorenzo
+ * @author Mezzapesa Beatrice, Papini Alessia, Pontellini Lorenzo
+ */
+/** Classe usata per la gestione delle pizze di tipo personalizzata e successivamente
+ *  anche per la gestione degli ordini.
  */
 public class pizzaPersonalizzata implements pizza{
 
@@ -21,40 +24,78 @@ public class pizzaPersonalizzata implements pizza{
         name = "personalizzata";
     }
     
+    /** Il cliente setta il numero delle pizze dello stesso tipo, che vuole ordinare
+     * @param n dove n è il numero settato
+     */
+    
     @Override
     public void setNPizze(int n) {
         this.nPizze = n;
     }
+    
+    /** Il cliente sceglie quali aggiunte fare alla sua pizza
+     * @param aggiunta dove aggiunta può essere base kamut, maxi e doppia margherita
+     */
     
     @Override
     public void setAggiunta(String aggiunta) {
         this.aggiunte.add(aggiunta);
     }
 
+    /** Ottengo cosa il cliente ha richiesto come aggiunte
+     * @return ArrayList<String> contente una stringa con la/e aggiunta/e
+     */
+    
     @Override
     public ArrayList<String> getAggiunte() {
         return aggiunte;
     }
+    
+    /** Ottengo il numero settato dal cliente di pizze dello stesso tipo
+     * @return int cioè il numero delle pizze
+     */
+    
     @Override
     public int getNPizze() {
         return nPizze;
     }
     
+    /** Ottengo il parametro personalizzata per gestire così al meglio gli ordini
+     * @return String con all'interno il parametro "personalizzata"
+     */
+    
     public String getName(){
         return name;
     }    
+    
+    /** Il cliente setta quale base vuole per la sua pizza personalizzata
+     * @param base è di tipo string e può essere bianca o margherita
+     */
     
     public void setBase(String base){
         this.base = base;
     }
     
+    /** Ottengo la base che il cliente ha scelto per la sua pizza personalizzata
+     * @return String il cui valore è bianca o margherita
+     */
+    
     public String getBase(){
         return base;
     } 
     
+    /** Il cliente setta quale/i condimento/i vuole per la sua pizza personalizzata
+     * @param cond è di tipo string e contiene il nome del condimento richiesto
+     */
+    
     public void setCondimento(String cond){
         condimenti.add(cond);
     }
+    
+    /** Ottengo il/i condimento/i che il cliente ha scelto per la sua pizza personalizzata
+     * @return ArrayList<String> con all'interno tutti i condimenti richiesti 
+     * (al massimo 4)
+     */
     
     public ArrayList<String> getCondimenti(){
         return condimenti;
--- Lib2/src/asw1021/pizze/pizzaStandard.java
+++ Lib2/src/asw1021/pizze/pizzaStandard.java
@@ -8,7 +8,10 @@ import java.util.ArrayList;
  */
 /**
  *
- * @author Lorenzo
+ * @author Mezzapesa Beatrice, Papini Alessia, Pontellini Lorenzo
+ */
+/** Classe usata per la gestione delle pizze di tipo standard e successivamente
+ *  anche per la gestione degli ordini.
  */
 public class pizzaStandard implements pizza{
     
@@ -18,28 +21,52 @@ public class pizzaStandard implements pizza{
     
     public pizzaStandard(){}
 
+    /** Il cliente sceglie quale tipo di pizza vuole tra quelle che gli vengono
+     *  elencate
+     * @param name è il nome della pizza
+     */
     public void setName(String name){
         this.name = name;
     }
+    
+    /** Il cliente setta il numero delle pizze dello stesso tipo, che vuole ordinare
+     * @param n dove n è il numero settato
+     */
     
     @Override
     public void setNPizze(int n) {
         this.npizze = n;
     }
 
+    /** Il cliente sceglie quali aggiunte fare alla sua pizza
+     * @param aggiunta dove aggiunta può essere base kamut, maxi e doppia margherita
+     */
+    
     @Override
     public void setAggiunta(String aggiunta) {
         this.aggiunte.add(aggiunta);
     }
+    
+    /** Ottengo cosa il cliente ha richiesto come aggiunte
+     * @return ArrayList<String> contente una stringa con la/e aggiunta/e
+     */
     
     @Override
     public ArrayList<String> getAggiunte(){
         return aggiunte;
     }
     
+    /** Ottengo il nome della pizza richiesta dal cliente
+     * @return String con all'interno il nome della pizza
+     */
+    
     public String getName(){
         return name;
     }
+    
+    /** Ottengo il numero settato dal cliente di pizze dello stesso tipo
+     * @return int cioè il numero delle pizze
+     */
     
     @Override
     public int getNPizze(){
